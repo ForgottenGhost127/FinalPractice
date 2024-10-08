1b7a91d737244ef4a020037c82067928b38961fd
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2024-10-09 00:55:24 +0200

    Start...Cubitos.

---
+++ Assets/Scripts/cubitoManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubitoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vamos a empezar a implementar la instanciación, la detección del click from mouse, el movimiento y el cambio de color.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
