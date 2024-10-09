27bf23f5b2174d35c8e185f1296a255059552f93
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2024-10-09 19:52:44 +0200

    Update...se acumulan los cubitos.

--- Assets/Scripts/cubitoManager.cs
+++ Assets/Scripts/cubitoManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class cubitoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject cubitoPref;
+    public int cubitoCount = 10;
+    public float moveSpeed = 5;
+
+    private List<GameObject> cubitos = new List<GameObject>();
+    private bool isMoving = false;
+
     void Start()
     {
-        //Vamos a empezar a implementar la instanciación, la detección del click from mouse, el movimiento y el cambio de color.
+        for (int i = 0; i < cubitoCount; i++) //Instanciando los cubos en posiciones aleatorias
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-10f, 10f), 0.5f, Random.Range(-10f, 10f));
+            GameObject cubito = Instantiate(cubitoPref, randomPos, Quaternion.identity);
+            cubitos.Add(cubito);
+
+            cubito.GetComponent<Renderer>().material.color = Color.magenta; //Los cubitos en "Idle" tendrán este color.
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0) && !isMoving) //Click Mouse y Raycast
+        {
+            Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(rayo, out hit))
+            {
+                Vector3 targetPos = hit.point;
+                isMoving = true;
+                StartCoroutine(MoveCubitos(targetPos));
+            }
+        }
         
     }
+
+    IEnumerator MoveCubitos(Vector3 target)
+    {
+        foreach(GameObject cube in cubitos)
+        {
+            cube.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        bool CubitosReachTarget = false;
+        //float separDistance = 0.5f; //Distancia de separación entre los cubos, porque se me aglomeraban después de coger la primera moneda y visualmente, sólo se veía un cubo algo deforme.
+
+        while(!CubitosReachTarget)
+        {
+            CubitosReachTarget = true;
+
+            foreach (GameObject cube in cubitos)
+            {
+                cube.transform.position = Vector3.MoveTowards(cube.transform.position, target, moveSpeed * Time.deltaTime);
+                if (Vector3.Distance(cube.transform.position, target) > 0.1f)
+                {
+                   CubitosReachTarget = false;
+                }
+            }
+            //for (int i = 0; i < cubitos.Count; i++)
+            //{
+            //    GameObject cube = cubitos[i];
+            //    Vector3 offset = new Vector3(Mathf.Sin(i + Time.time) * separDistance, 0, Mathf.Cos(i + Time.time) * separDistance);
+            //    Vector3 moveTarget = target + offset;
+            //    cube.transform.position = Vector3.MoveTowards(cube.transform.position, moveTarget, moveSpeed * Time.deltaTime);
+            //    if (Vector3.Distance(cube.transform.position, moveTarget) > 0.1f)
+            //    {
+            //        CubitosReachTarget = false;
+            //    }
+            //}
+            yield return null;
+        }
+
+        foreach (GameObject cube in cubitos)
+        {
+            cube.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+
+        isMoving = false; 
+    }
+
 }
